852d778f25cecf84991ab331e9413fda3291ec70
Radosław Cymer <cymerrad@gmail.com>
2020-05-17 15:19:14 +0200

    Fix for hyphens

--- HOME/.local/bin/clone-git-ssh.sh
+++ HOME/.local/bin/clone-git-ssh.sh
@@ -1,16 +1,27 @@
 #!/bin/bash
 
-url=$1
-[ -z "$url" ] && echo "Provide github repo url" && exit 1
+help_text="\n\tclone-git-ssh [git-clone opts] <repository>\n\n\tUse ssh instead of http(s) for git remote operationos.\n\tClones repository into nested directory of user_name/repo_name."
+print_help() {
+    echo $1
+    echo
+    echo -e $help_text
+}
+
+last="${@: -1}"
+init_count=$(( $# - 1 ))
+init=${@: 1:$init_count}
+
+url=${last}
+[ -z "$url" ] && print_help "Provide github repo url" && exit 1
 
 #accepts /blob/commit/path/to/somewhere at the end
-gh_re='^https?://github.com/(\w+)/(\w+)/?.*$'
+gh_re='^https?://github.com/([a-zA-Z0-9_-]+)/([a-zA-Z0-9_-]+)(/.*)?$'
 
 repo=`echo $url | sed -rne "\|${gh_re}|s|${gh_re}|\1/\2|p"`
-[ -z "$repo" ] && echo "Invalid github repo url" && exit 1
+[ -z "$repo" ] && print_help "Invalid github repo url" && exit 1
 
 url_ssh="git@github.com:${repo}"
 
-echo git clone ${url_ssh} ${repo}
-git clone ${url_ssh} ${repo}
+echo git clone ${init} ${url_ssh} ${repo}
+#git clone ${init} ${url_ssh} ${repo}
 
