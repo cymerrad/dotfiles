ca01768d18cfbe702052748113d06aa5381e4bca
Radosław Cymer <cymerrad@gmail.com>
2020-07-23 00:33:18 +0200

    Regexp fix

--- HOME/.local/bin/clone-git-ssh.sh
+++ HOME/.local/bin/clone-git-ssh.sh
@@ -4,9 +4,11 @@ url=$1
 [ -z "$url" ] && echo "Provide github repo url" && exit 1
 
 #accepts /blob/commit/path/to/somewhere at the end
-gh_re='^https?://github.com/(\w+)/(\w+)/?.*$'
+gh_re='^https?://github\.com/([0-9A-Za-z\-]+)/([0-9A-Za-z\-]+)(/.*)?$'
 
-repo=`echo $url | sed -rne "\|${gh_re}|s|${gh_re}|\1/\2|p"`
+echo $url sed -rne "\%${gh_re}%s%${gh_re}%\2/\2%p"
+
+repo=`echo $url | sed -rne "\%${gh_re}%s%${gh_re}%\1/\2%p"`
 [ -z "$repo" ] && echo "Invalid github repo url" && exit 1
 
 url_ssh="git@github.com:${repo}"
